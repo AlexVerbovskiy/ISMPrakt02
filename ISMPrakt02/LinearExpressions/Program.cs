e925e14cdc037dafc509a45336d2219261b76c50
Alexandr <git>
2021-10-16 14:15:12 +0300

    Завантаження другого дз

---
+++ ISMPrakt02/LinearExpressions/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LinearExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a = ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("b = ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("c = ");
+            double c = Convert.ToDouble(Console.ReadLine());
+            Console.Write("d = ");
+            double d = Convert.ToDouble(Console.ReadLine());
+            double x, y, z, r;
+            x = (a + 2 * b - c + d) / (c * d) + (a + b) / (c - d) - Math.Pow(a, 2) / Math.Pow(b, 2);
+            y = 5*(a + b)*( c - d) / (1.0*c/2) + (Math.Pow(d, 2)*( Math.Pow(a, 2) - Math.Pow(b, 2)))/(b-a);
+            z = (Math.Pow(x * x - 2 * x, 3) - 4 * (Math.Pow(x, 4) + 1)) * (1 - b)/(5*a+3*b);
+            r = (1.0 * a / 2 + 3.0 * b / 4 - 7.0 / 5) / (3 * c + 1) + (1.0 / (a - c));
+            Console.WriteLine($"x = {x}, y = {y}, z = {z}, r = {r}");
+        }
+    }
+}
---
+++ ISMPrakt02/QuadraticEquation/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace QuadraticEquation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a = ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("b = ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("c = ");
+            double c = Convert.ToDouble(Console.ReadLine());
+            Console.OutputEncoding = Encoding.UTF8;
+            double D = b * b - 4 * a * c;
+            Console.WriteLine($"Дискримінант: {D}");
+            if (D < 0) {
+                Console.WriteLine("Дискримінант менше 0!");
+            }else if (D == 0)
+            {
+                Console.WriteLine($"Корінь: {-b*1.0/(2*a)}");
+            }
+            else
+            {
+                Console.WriteLine($"Корінь 1: {(-b * 1.0 + Math.Sqrt(D)) / (2 * a)}");
+                Console.WriteLine($"Корінь 2: {(-b * 1.0 - Math.Sqrt(D)) / (2 * a)}");
+            }
+        }
+    }
+}
